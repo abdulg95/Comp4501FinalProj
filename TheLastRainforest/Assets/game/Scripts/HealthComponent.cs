dd60a5656b984d4d06ff1816a7384c8b697d9622
Conrad Cameron <darkstars42@gmail.com>
2018-03-04 16:59:13 -0500

    adding components to units
    units now have apprpriate components, movement needs to be finished,  abilities need to be coded individually

---
+++ TheLastRainforest/Assets/game/Scripts/HealthComponent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour {
+
+
+    public int maximumHealth;
+    public int currentHealth;
+    public int regenerationDelay; // the higher this is the less the unit will regen
+    private int counter;
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsDead())
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            if(counter == regenerationDelay && currentHealth < maximumHealth)
+            {
+                currentHealth++;
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+		
+	}
+
+    public bool IsDead()
+    {
+        return (currentHealth <= 0);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
---
+++ TheLastRainforest/Assets/game/Scripts/MovementComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour {
+
+    public float maxSpeed;
+    public float currentSpeed;
+    public Vector3 target;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveTo(Vector3 t)
+    {
+        target = t;
+    }
+}
