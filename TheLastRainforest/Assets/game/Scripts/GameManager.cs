dc5e1b5f173289d1ae53207d9224c1592d603530
abdulg95 <abdulg1995@gmail.com>
2018-03-03 21:15:13 -0500

    GameManager and Tree Assets
    Created 2 tree models
    Added GameObject script
    Added a few methods to
    GameObject script
    Built sample terrain

---
+++ TheLastRainforest/Assets/game/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	//declare and initialize gamemanager instance to null
+	public static GameManager instance = null;
+
+	//define startup values here e.g hp
+
+	void Awake(){
+		//enforce singleton we only need one gamemanager
+		if (instance == null) {
+			instance = this;		
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
