43275eb7a48410c0dc13c0b81803b64f66fe5380
Abdulganiyu Yusufu <abdulg1995@gmail.com>
2018-04-01 17:36:21 -0400

    merge
    merged changes from both branches. Including gold resource, abilities and leader following

--- TheLastRainforest/Assets/game/Scripts/GameManager.cs
+++ TheLastRainforest/Assets/game/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     //declare and initialize gamemanager instance to null
     public static GameManager instance = null;
     public GameObject prefab;
     private static GameObject[] gos;
+    public double gold = 0;
 
     //define startup values here e.g hp
 
@@ -30,21 +32,37 @@ public class GameManager : MonoBehaviour {
 
     }
 
+    void UpdateGold()
+    {
+         gold++;
+    }
+
     public  GameObject[] GetGos()
     {
         return gos;
     }
 
+    public void DeductGold()
+    {
+        gold-=15;
+    }
 
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         
     }
 
-	// Update is called once per frame
-	void Update () {
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Gold: "+gold);
+    }
 
+    // Update is called once per frame
+    void Update () {
+        gold += 0.5;
+       
 	}
 }
